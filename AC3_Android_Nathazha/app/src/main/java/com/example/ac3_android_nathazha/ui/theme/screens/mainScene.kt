3ae9fac0c94719e1997689d84cb773f03aa8b123
Nathazha27 <nathazha.bernales@estudiant.enti.cat>
2025-10-13 18:33:34 +0200

    Creando clases

---
+++ AC3_Android_Nathazha/app/src/main/java/com/example/ac3_android_nathazha/ui/theme/screens/mainScene.kt
@@ -0,0 +1,23 @@
+package com.example.ac3_android_nathazha.ui.theme.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ac3_android_nathazha.ui.theme.models.mainStart
+
+class mainScene {
+
+
+}
