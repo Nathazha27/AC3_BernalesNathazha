a512affc69e07a202b3dcf63102d9c26c465bc85
Nathazha27 <nathazha.bernales@estudiant.enti.cat>
2025-10-18 13:21:51 +0200

    Cambio de proyecto a versión nueva (Narwhal)

--- AC3_Android_Nathazha/app/src/main/java/com/example/ac3_android_nathazha/models/mainStart.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ac3_android_nathazha.models
-
-import androidx.annotation.StringRes
-
-data class mainStart(
-    @StringRes val titulo: Int,
-    var score: Int
-)
---
+++ app/src/main/java/com/example/ac3_android_nathazha/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.ac3_android_nathazha
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ac3_android_nathazha.ui.theme.AC3_Android_NathazhaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AC3_Android_NathazhaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AC3_Android_NathazhaTheme {
+        Greeting("Android")
+    }
+}
